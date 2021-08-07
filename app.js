788655a129608fde78d11ff82e54f42bf9a14b5d
凉风有信 <3042146237@qq.com>
2021-08-07 14:44:20 +0800

    feat:日志规范封装

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const json = require('koa-json')
 const onerror = require('koa-onerror')
 const bodyparser = require('koa-bodyparser')
 const logger = require('koa-logger')
+const log4js = require('./utils/log4')
 
 const index = require('./routes/index')
 const users = require('./routes/users')
@@ -17,7 +18,7 @@ app.use(bodyparser({
   enableTypes:['json', 'form', 'text']
 }))
 app.use(json())
-// app.use(logger())
+app.use(logger())
 app.use(require('koa-static')(__dirname + '/public'))
 
 app.use(views(__dirname + '/views', {
@@ -26,10 +27,8 @@ app.use(views(__dirname + '/views', {
 
 // logger
 app.use(async (ctx, next) => {
-  const start = new Date()
   await next()
-  const ms = new Date() - start
-  console.log(`${ctx.method} ${ctx.url} - ${ms}ms`)
+  log4js.info('log output')
 })
 
 // routes
@@ -38,7 +37,7 @@ app.use(users.routes(), users.allowedMethods())
 
 // error-handling
 app.on('error', (err, ctx) => {
-  console.error('server error', err, ctx)
+  log4js.error(err.stack)
 });
 
 module.exports = app
