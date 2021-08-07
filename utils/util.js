1e32e79d59ada1ec2da7153a5df6bc6625cf6263
凉风有信 <3042146237@qq.com>
2021-08-07 15:36:22 +0800

    feat:工具函数的封装

---
+++ utils/util.js
@@ -0,0 +1,45 @@
+/**
+ * @description 工具函数
+ * @author 凉风有信、
+ */
+
+const log4js = require('./log4j')
+const CODE = {
+    SUCCESS: 200,
+    PARAM_ERROR: 10001, // 参数错误
+    USER_ACCOUNT_ERROR: 20001, //账号或密码错误
+    USER_LOGIN_ERROR: 30001, // 用户未登录
+    BUSINESS_ERROR: 40001, //业务请求失败
+    AUTH_ERROR: 500001, // 认证失败或TOKEN过期
+}
+module.exports = {
+    /**
+     * 分页结构封装
+     * @param {number} pageNum 
+     * @param {number} pageSize 
+     */
+    pager({ pageNum = 1, pageSize = 10 }) {
+        pageNum *= 1;
+        pageSize *= 1;
+        const skipIndex = (pageNum - 1) * pageSize;
+        return {
+            page: {
+                pageNum,
+                pageSize
+            },
+            skipIndex
+        }
+    },
+    success(data = '', msg = '', code = CODE.SUCCESS) {
+        log4js.debug(data);
+        return {
+            code, data, msg
+        }
+    },
+    fail(msg = '', code = CODE.BUSINESS_ERROR, data = '') {
+        log4js.debug(msg);
+        return {
+            code, data, msg
+        }
+    }
+}
