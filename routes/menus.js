05928e2bb64cb755a029dce593df465e090cf351
凉风有信 <3042146237@qq.com>
2021-08-11 20:49:17 +0800

    feat:菜单列表递归实现

--- routes/menus.js
+++ routes/menus.js
@@ -9,6 +9,7 @@ const Menu = require('./../models/menuSchema')
 
 router.prefix('/api/menu')
 
+// 菜单增删：
 router.post('/operate', async (ctx, next) => {
   const { _id, active, ...params } = ctx.request.body
   let res, info
@@ -35,5 +36,41 @@ router.post('/operate', async (ctx, next) => {
     ctx.body = util.fail(error.stack)
   }
 })
+
+// 菜单列表查询：
+router.get('/list', async (ctx, next) => {
+  const { menuName, menuState } = ctx.request.query
+  const params = {}
+  if (menuName) {
+    params.menuName = menuName
+  }
+  if (menuState) {
+    params.menuState = menuState
+  }
+  const rootList = await Menu.find(params) || []
+  const primissionList = getTreeMenu(rootList, null, [])
+  ctx.body = util.success(primissionList)
+})
+
+// 递归拼接树形列表
+function getTreeMenu (rootList, id, list) {
+  for (let i = 0; i < rootList.length; i++) {
+      let item = rootList[i]
+      if (String(item.parentId.slice().pop()) == String(id)) {
+          list.push(item._doc)
+      }
+  }
+  list.map(item => {
+      item.children = []
+      getTreeMenu(rootList, item._id, item.children)
+      if (item.children.length == 0) {
+          delete item.children;
+      } else if (item.children.length > 0 && item.children[0].menuType == 2) {
+          // 快速区分按钮和菜单，用于后期做菜单按钮权限控制
+          item.action = item.children;
+      }
+  })
+  return list;
+}
 
 module.exports = router
