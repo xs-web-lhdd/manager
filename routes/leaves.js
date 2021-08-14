7f739e73908bd27c8bd639c18c875acfc36ce18f
凉风有信 <3042146237@qq.com>
2021-08-14 15:08:28 +0800

    feat:休假申请服务端实现&较为麻烦

--- routes/leaves.js
+++ routes/leaves.js
@@ -5,7 +5,7 @@
 
 const router = require('koa-router')()
 const util = require('../utils/util')
-const { find } = require('./../models/leaveSchema')
+const Dept = require('./../models/deptSchema')
 const Leave = require('./../models/leaveSchema')
 
 router.prefix('/api/leave')
@@ -33,6 +33,57 @@ router.get('/list', async (ctx, next) => {
     })
   } catch (error) {
     ctx.body = util.fail(`数据库查询失败：${error.stack}`)
+  }
+})
+
+// 休假申请---创建、作废
+router.post('/operate', async (ctx, next) => {
+  const { id, action, ...params } = ctx.request.body
+  const authorization = ctx.request.headers.authorization
+  let { data } = util.decoded(authorization)
+  if (action === 'create') {
+    // 生成申请单号
+    let orderNo = 'XJ'
+    orderNo += util.formateDate(new Date(), 'yyyyMMdd')
+    const total = await Leave.countDocuments()
+    params.orderNo = orderNo + total
+    // 获取用户部门ID
+    let DeptId = data.deptId.pop()
+    // 查找用户负责人信息
+    let dept = await Dept.findById(DeptId)
+    // 获取人事部门和财务部门负责人信息
+    let userList = await Dept.find({ deptName: { $in: ['人事部门', '财务部门'] } })
+    // 审批人
+    let auditUsers = dept.userName
+    // 当前审批人
+    let curAuditUserName = dept.userName
+    // 审批流
+    let auditFlows = [
+      { userId: dept.userId, userName: dept.userName, userEmail: dept.userEmail }
+    ]
+    userList.map(item => {
+      auditFlows.push({
+        userId: item.userId, userName: item.userName, userEmail: item.userEmail
+      })
+      auditUsers += ',' + item.userName
+    })
+
+    params.auditUsers = auditUsers
+    params.curAuditUserName = curAuditUserName
+    params.auditFlows = auditFlows
+    params.applyUser = {
+      userId: data.userId,
+      userName: data.userName,
+      userEmail: data.userEmail
+    } // 得到用户
+
+    let res = await Leave.create(params)
+    ctx.body = util.success('', '创建成功')
+
+  } else {
+    // 软删除
+    let res = await Leave.findByIdAndUpdate(id, { applyState: 5 })
+    ctx.body = util.success('', '操作成功')
   }
 })
 
--- utils/util.js
+++ utils/util.js
@@ -69,6 +69,27 @@ module.exports = {
             item.action = item.children;
         }
     })
-    return list;
-  }
+        return list;
+    },
+    formateDate(date, rule) {
+        let fmt = rule || 'yyyy-MM-dd hh:mm:ss'
+        if (/(y+)/.test(fmt)) {
+            fmt = fmt.replace(RegExp.$1, date.getFullYear())
+        }
+        const o = {
+            // 'y+': date.getFullYear(),
+            'M+': date.getMonth() + 1,
+            'd+': date.getDate(),
+            'h+': date.getHours(),
+            'm+': date.getMinutes(),
+            's+': date.getSeconds()
+        }
+        for (let k in o) {
+            if (new RegExp(`(${k})`).test(fmt)) {
+                const val = o[k] + '';
+                fmt = fmt.replace(RegExp.$1, RegExp.$1.length == 1 ? val : ('00' + val).substr(val.length));
+            }
+        }
+        return fmt;
+    }
 }
