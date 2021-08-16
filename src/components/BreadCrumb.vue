6170115ad6fd9b4b880f21fe95f35420d2b75fbc
凉风有信、 <3042146237@qq.com>
2021-08-16 18:16:11 +0800

    fix:修复在面包屑上点击回首页跳到404页

--- src/components/BreadCrumb.vue
+++ src/components/BreadCrumb.vue
@@ -1,7 +1,7 @@
 <template>
     <el-breadcrumb separator-class="el-icon-arrow-right">
         <el-breadcrumb-item v-for="(item, index) in breadList" :key="index">
-          <router-link to="/welcome" v-if="index === 0">{{item.meta.title}}</router-link>
+          <router-link to="/Welcome" v-if="index === 0">{{item.meta.title}}</router-link>
           <span v-else>{{item.meta.title}}</span>
         </el-breadcrumb-item>
     </el-breadcrumb>
